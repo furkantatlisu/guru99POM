70598dcbe0a4038669b3090112bd6cede3343a4c
furkantatlisu <furkan58tatlisu@gmail.com>
2022-08-03 01:39:37 +0300

    updated

--- src/test/java/Test99GuruLogin.java
+++ src/test/java/Test99GuruLogin.java
@@ -9,29 +9,18 @@ import pages.Guru99Login;
 import java.util.concurrent.TimeUnit;
 
 public class Test99GuruLogin {
-
     String driverPath = "C:\\geckodriver.exe";
-
     WebDriver driver;
-
     Guru99Login objLogin;
-
     Guru99HomePage objHomePage;
 
     @BeforeTest
-
     public void setup() {
-
         System.setProperty("webdriver.gecko.driver", driverPath);
-
         driver = new FirefoxDriver();
-
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
         driver.get("http://demo.guru99.com/V4/");
-
     }
-
     /**
      * This test case will login in http://demo.guru99.com/V4/
      * <p>
@@ -43,30 +32,21 @@ public class Test99GuruLogin {
      */
 
     @Test(priority = 0)
-
     public void test_Home_Page_Appear_Correct() {
-
         //Create Login Page object
-
         objLogin = new Guru99Login(driver);
 
         //Verify login page title
-
         String loginPageTitle = objLogin.getLoginTitle();
-
         Assert.assertTrue(loginPageTitle.toLowerCase().contains("guru99 bank"));
 
         //login to application
-
         objLogin.loginToGuru99("mgr123", "mgr!23");
 
         // go the next page
-
         objHomePage = new Guru99HomePage(driver);
 
         //Verify home page
-
         Assert.assertTrue(objHomePage.getHomePageDashboardUserName().toLowerCase().contains("manger id : mgr123"));
-
     }
 }
